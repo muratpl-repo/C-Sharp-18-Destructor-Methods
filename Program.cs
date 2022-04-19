76762b1926fe97914c5ef8335e27c0fa5e75e836
muratpala007 <muratpala007@gmail.com>
2022-04-19 16:37:16 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _59DersDeconstructorMetotu
+{
+    class Program
+    {   
+        class Insan
+        {
+            public Insan()
+            {
+                Console.WriteLine("Obje olusturuldu...");
+            }
+
+            ~Insan() //Tilda isareti ile deconsturctor metotu kurulur.
+            {
+                Console.WriteLine("Obje yok edildi...");
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            objeleriOlustur();
+            GC.Collect(); //Cop toplayici olarak gecer.
+        }
+
+        static void objeleriOlustur()
+        {
+            Insan i2 = new Insan();
+            Insan i1 = new Insan();
+        }
+    }
+}
